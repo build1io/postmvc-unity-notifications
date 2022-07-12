cc32cc934a79baac2b83f3833babba612cf712e3
George Markosian <georgemarkosian@gmail.com>
2022-07-12 10:53:28 +0200

    Show in foreground option added (iOS only).

--- Impl/NotificationsControllerIOS.cs
+++ Impl/NotificationsControllerIOS.cs
@@ -225,7 +225,7 @@ namespace Build1.PostMVC.Unity.Modules.Notifications.Impl
                 Subtitle = notification.subTitle,
                 Body = notification.text,
 
-                ShowInForeground = true,
+                ShowInForeground = notification.ShowInForeground,
                 ForegroundPresentationOption = PresentationOption.Alert | PresentationOption.Badge | PresentationOption.Sound,
                 Badge = 1,
                 CategoryIdentifier = "default_category",
