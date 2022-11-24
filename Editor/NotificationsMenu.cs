1cfda32dd3951e2a52d8d90dee633936fcd55c3e
George Markosian <georgemarkosian@gmail.com>
2022-11-24 11:57:11 +0100

    Notifications menu messagging improvements.

--- Editor/NotificationsMenu.cs
+++ Editor/NotificationsMenu.cs
@@ -2,7 +2,6 @@
 
 using Build1.PostMVC.Unity.Notifications.Impl;
 using UnityEditor;
-using UnityEngine;
 
 namespace Build1.PostMVC.Unity.Notifications.Editor
 {
@@ -12,15 +11,14 @@ namespace Build1.PostMVC.Unity.Notifications.Editor
         public static void View()
         {
             var status = NotificationsControllerEditor.GetAuthorizationStatusStatic();
-            EditorUtility.DisplayDialog("Notifications", $"Current status: {status}", "Ok");
+            EditorUtility.DisplayDialog("Notifications", $"Current status: {status}.", "Ok");
         }
         
         [MenuItem("Tools/Build1/Notifications/Reset Editor Authorization", false, 2114)]
         public static void Reset()
         {
             NotificationsControllerEditor.ResetAuthorization();
-            
-            Debug.Log("Notifications: Editor authorization status reset");
+            EditorUtility.DisplayDialog("Notifications", "Editor authorization status reset.", "Ok");
         }
     }
 }
