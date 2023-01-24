c703baf7d1dbe1a0e9cfbeea7ae7a4aab5cd2200
George Markosian <georgemarkosian@gmail.com>
2023-01-24 15:57:54 +0100

    Initialization command optimization.

--- Commands/NotificationsInitializeCommand.cs
+++ Commands/NotificationsInitializeCommand.cs
@@ -1,23 +1,18 @@
 using Build1.PostMVC.Core.MVCS.Commands;
 using Build1.PostMVC.Core.MVCS.Events;
 using Build1.PostMVC.Core.MVCS.Injection;
-using Build1.PostMVC.Unity.App.Modules.Logging;
 
 namespace Build1.PostMVC.Unity.Notifications.Commands
 {
     public sealed class NotificationsInitializeCommand : Command<NotificationsSettings>
     {
-        [Log(LogLevel.Warning)] public ILog                     Log                     { get; set; }
-        [Inject]                public IEventDispatcher         Dispatcher              { get; set; }
-        [Inject]                public INotificationsController NotificationsController { get; set; }
+        [Inject] public IEventDispatcher         Dispatcher              { get; set; }
+        [Inject] public INotificationsController NotificationsController { get; set; }
 
         public override void Execute(NotificationsSettings settings)
         {
             if (NotificationsController.Initialized)
-            {
-                Log.Warn("Notifications already initialized");
                 return;
-            }
 
             Retain();
 
