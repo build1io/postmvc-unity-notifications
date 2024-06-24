c0800fa6d81d9e74590e6a62bcca7bc55dc7aefa
George Markosian <georgemarkosian@gmail.com>
2024-06-24 16:07:59 +0200

    Native settings opening on Android fixed.

--- Impl/NotificationsControllerAndroid.cs
+++ Impl/NotificationsControllerAndroid.cs
@@ -111,7 +111,7 @@ namespace Build1.PostMVC.Unity.Notifications.Impl
 
                 using var uriClass = new AndroidJavaClass("android.net.Uri");
                 using var uriObject = uriClass.CallStatic<AndroidJavaObject>("fromParts", "package", packageName, null);
-                using var intentObject = new AndroidJavaObject("android.content.Intent", "android.settings.APP_NOTIFICATION_SETTINGS", uriObject);
+                using var intentObject = new AndroidJavaObject("android.content.Intent", "android.settings.APPLICATION_DETAILS_SETTINGS", uriObject);
                 intentObject.Call<AndroidJavaObject>("addCategory", "android.intent.category.DEFAULT");
                 intentObject.Call<AndroidJavaObject>("setFlags", 0x10000000);
                 currentActivityObject.Call("startActivity", intentObject);
